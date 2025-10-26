9e754214c9e17c629a69120910c829e73744f099
Mike Ion <mikeion@umich.edu>
2025-10-25 20:36:16 -0700

    Update basePath for organization GitHub Pages deployment
    - Remove basePath since deploying to root domain
    - New URL will be pentagon-game.github.io instead of mikeion.github.io/pentagon-game/

--- pentagon-webapp/next.config.ts
+++ pentagon-webapp/next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/pentagon-game' : '',
+  basePath: '',  // No basePath needed anymore!
   images: {
     unoptimized: true
   }
